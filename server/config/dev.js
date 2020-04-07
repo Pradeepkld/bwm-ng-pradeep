8a2b5d88f6ffd03c604b1f5b5e116f10f3360250
DEEPU <DEEPU@DEEPUHOME>
2020-04-07 06:27:09 +0530

    mlab cred added+ router link issue fix

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,4 +1,8 @@
 module.exports = {
-  DB_URI: 'mongodb://test:testtest1@ds055722.mlab.com:55722/bwm-ng-pradeep',
-  SECRET: 'deepu'
+  DB_URI:
+    //"mongodb://pradeepkld:prad1982@ds055722.mlab.com:55722/bwm-ng-pradeep",
+    //"mongodb://test:testtest1@ds055722.mlab.com:55722/bwm-ng-pradeep",
+    "mongodb://pradeep:deepu1982@ds119795.mlab.com:19795/ng-airbnbstyle",
+
+  SECRET: "deepu",
 };
